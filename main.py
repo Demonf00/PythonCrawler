9eecf8d627505996cec407ce876a285c7e6d01df
Demonf00 <freddy.zhao@mail.utoronto.ca>
2022-05-07 17:47:22 -0400

    Robust

--- main.py
+++ main.py
@@ -18,12 +18,12 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 
 class target:
-	def __init__(self,\
-				 url,\
-				 header = {"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36", "cookie":"thw=cn; ??????????????????????????????"},\
-				 others):
+	def __init__(self, url, header, others):
 		self.url = url
-		self.header = header
+		if header == None:
+			self.header = {"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36", "cookie":"thw=cn; ??????????????????????????????"}
+		else:
+			self.header = header
 
 
 	def downloadPage(self):
@@ -47,27 +47,35 @@ class target:
 			return None
 		return title
 
-	
 
 
 def download_from_url(url, dst):
-	response = requests.get(url, stream=True)
-	file_size = int(response.headers['content-length'])
-	if os.path.exists(dst):
-		first_byte = os.path.getsize(dst)
-	else:
-		first_byte = 0
-	if first_byte >= file_size:
-		return file_size
-	header = {"Range": f"bytes={first_byte}-{file_size}"}
-	pbar = tqdm(
-		total=file_size, initial=first_byte,
-		unit='B', unit_scale=True, desc=dst)
-	req = requests.get(url, headers=header, stream=True)
-	with(open(dst, 'ab')) as f:
-		for chunk in req.iter_content(chunk_size=1024):
-			if chunk:
-				f.write(chunk)
-				pbar.update(1024)
-	pbar.close()
-	return file_size
+	try:
+		response = requests.get(url, stream=True)
+		file_size = int(response.headers['content-length'])
+		if file_size != 259:
+			if os.path.exists(dst):
+				first_byte = os.path.getsize(dst)
+			else:
+				first_byte = 0
+			if first_byte >= file_size:
+				return file_size
+			header = {"Range": f"bytes={first_byte}-{file_size}"}
+			pbar = tqdm(
+				total=file_size, initial=first_byte,
+				unit='B', unit_scale=True, desc=dst)
+			req = requests.get(url, headers=header, stream=True)
+			with(open(dst, 'ab')) as f:
+				for chunk in req.iter_content(chunk_size=1024):
+					if chunk:
+						f.write(chunk)
+						pbar.update(1024)
+			pbar.close()
+			return file_size
+		else:
+			return file_size
+	except:
+		return None
+
+
+
